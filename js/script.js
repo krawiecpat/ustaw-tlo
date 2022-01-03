f325f8d1c3655051aaa70e983a2fd72f2d1b5859
krawiecpat <krawiecpat9@gmail.com>
2022-01-03 15:07:16 +0100

    ustaw tło

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log('Ustaw tło paragrafów');
+
+const setBackground = () => {
+    let p1 = document.querySelector('.first');
+    let p2 = document.querySelector('.last');
+
+    p1.style.backgroundColor = 'red';
+    p2.style.backgroundColor = 'yellow';
+}
+
+let btnSetBackground = document.getElementById('set-background');
+
+btnSetBackground.addEventListener('click', setBackground);
